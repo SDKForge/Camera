72e007ab2ac145938d38cfe44e41892d99ee2db2
Volodymyr Nevmerzhytskyi <azazellj@gmail.com>
2025-08-07 18:21:18 +0300

    initial release: Camera

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -43,7 +43,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "SDKForgeTemplate"
+rootProject.name = "SDKForge-Camera"
 
 include(":app-android")
 include(":app-shared")
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 kotlin {
     androidLibrary {
-        namespace = "dev.sdkforge.template"
+        namespace = "dev.sdkforge.camera"
     }
 
     // export dependencies for iOS
