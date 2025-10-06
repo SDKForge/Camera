c7b9042cf2d23c091cd4071f3e183e86b5db93ec
yevhen.kryvoshei <ekryvoshey@gmail.com>
2025-10-06 14:02:22 +0300

    WIP. Added selection buttons group

--- app-shared/src/commonMain/kotlin/dev/sdkforge/camera/app/ScannerScreen.kt
+++ app-shared/src/commonMain/kotlin/dev/sdkforge/camera/app/ScannerScreen.kt
@@ -1,6 +1,7 @@
 package dev.sdkforge.camera.app
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,12 +9,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FlashOff
 import androidx.compose.material.icons.filled.FlashOn
 import androidx.compose.material.icons.filled.FlipCameraAndroid
 import androidx.compose.material.icons.filled.History
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,15 +30,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import dev.sdkforge.camera.domain.ScanResult
 import dev.sdkforge.camera.ui.CameraController
 import dev.sdkforge.camera.ui.CameraView
+import kotlinx.coroutines.launch
 
 @Composable
 fun ScannerScreen(
@@ -48,16 +59,68 @@ fun ScannerScreen(
             cameraController = cameraController,
             modifier = Modifier.fillMaxSize(),
         )
-        ButtonsOverlay(
-            controller = cameraController,
+        ModeSwitcher(
             modifier = modifier.padding(top = innerPadding.calculateTopPadding()),
-            onHistoryClicked = { isHistoryDialogShown = !isHistoryDialogShown },
         )
-        ScansHistoryDialog(
-            showDialog = isHistoryDialogShown,
-            scans = scans,
-            onDismiss = { isHistoryDialogShown = false },
-        )
+//        ButtonsOverlay(
+//            controller = cameraController,
+//            modifier = modifier.padding(top = innerPadding.calculateTopPadding()),
+//            onHistoryClicked = { isHistoryDialogShown = !isHistoryDialogShown },
+//        )
+//        ScansHistoryDialog(
+//            showDialog = isHistoryDialogShown,
+//            scans = scans,
+//            onDismiss = { isHistoryDialogShown = false },
+//        )
+    }
+}
+
+@Composable
+fun ModeSwitcher(
+    modifier: Modifier = Modifier,
+) {
+    val scope = rememberCoroutineScope()
+    val state = rememberLazyListState()
+    val listOfItems = remember { mutableStateListOf(MenuItem("Camera"), MenuItem("Scanner")) }
+    val cardBackgroundColor = remember { mutableStateOf(Color.Transparent) }
+
+    val onClickEvent: (Int) -> Unit = {
+        listOfItems.forEachIndexed { index, item ->
+            item.isSelected = index == it
+            cardBackgroundColor.value = if (item.isSelected) Color.White else Color.Transparent
+            println("MY_LOG --- index: $index -- item: $item")
+            scope.launch {
+                state.animateScrollToItem(index)
+            }
+        }
+    }
+    LazyRow(
+        state = state,
+        modifier = modifier
+            .padding(8.dp).fillMaxWidth(),
+    ) {
+        itemsIndexed(listOfItems) { index, item ->
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = if (item.isSelected) {
+                        Color.White
+                    } else {
+                        Color.Transparent
+                    },
+                ),
+                shape = RoundedCornerShape(
+                    size = 24.dp,
+                ),
+                onClick = {
+                    onClickEvent.invoke(index)
+                },
+            ) {
+                Text(
+                    text = item.name,
+                    modifier = Modifier.padding(horizontal = 24.dp, vertical = 8.dp),
+                )
+            }
+        }
     }
 }
 
@@ -154,3 +217,5 @@ fun ScansHistoryDialog(
         }
     }
 }
+
+data class MenuItem(val name: String, var isSelected: Boolean = false)
